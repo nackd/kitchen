748634910ecd040654a7e7ae87286dbfc1938721
Jose M. Prieto <jmprieto@emergya.com>
2011-12-19 17:41:48 +0100

    groups: use chef roles
    Rename some clases & fields. Change routes.

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,17 @@
-class Role < ChefBase    
+class Role < ChefBase
+  def nodes
+    [] # TODO
+  end
 
+  def available_roles
+    [] # TODO
+  end
+
+  def available_recipes
+    [] # TODO
+  end
+
+  def extended_run_list
+    [] # TODO
+  end
 end
