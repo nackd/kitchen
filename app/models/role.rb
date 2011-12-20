d553435146edb0a40b2f1d43e2a3f2aacf30f92f
Jose M. Prieto <jmprieto@emergya.com>
2011-12-20 13:41:32 +0100

    role: default attribute values

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,22 @@
 class Role < ChefBase
   def initialize(attributes={})
-    attributes = { "name" => nil,
-                   "nodes_list" => [] }.merge(attributes)
+    attributes.reject! { |key, value|
+      ["chef_type", "chef_name"].include? key.to_s
+    }
+    attributes = {
+      "name"                => nil,
+      "chef_type"           => "role",
+      "json_class"          => "Chef::Role",
+      "default_attributes"  => {},
+      "description"         => nil,
+      "run_list"            => [],
+      "override_attributes" => {}
+    }.merge(attributes)
     super(attributes)
+  end
+
+  def nodes_list
+    [] # TODO
   end
 
   def nodes
