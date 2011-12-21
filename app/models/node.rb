9e7eb8d1f799f9756796b952aa3c0ba9da82be09
Jose M. Prieto <jmprieto@emergya.com>
2011-12-21 18:59:44 +0100

    roles: assign nodes on creation and update, unassign on delete

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,56 +1,61 @@
 class Node < ChefBase
-  
   [:patform, :hostname, :ip].each do |method|
    define_method method do
       self.automatic["method"]
    end
-  end   
+  end
 
-  
-  def platform_version 
+  def platform_version
     "#{self.automatic["platform"]} #{self.automatic["platform_version"]}"
-  end     
- 
-  
+  end
+
   def status
     self.automatic["uptime"].blank? ? "off" : "on"
-  end 
-  
+  end
+
   def error?
     self.normal["last_run_error"]
-  end  
-  
+  end
+
   def error
     self.normal["last_run_status_msg"]
-  end   
-  
+  end
+
   def users
     self.automatic["users"].map {|u| u["username"] } if self.automatic["users"]
-  end 
-  
+  end
+
   def users_list
     self.users.join(", ")
   end
-  
-  def self.find_by_user(user) 
+
+  def self.find_by_user(user)
     self.class.find(:all, :user => user)
   end
-  
-  
+
   def extended_run_list
     rl = []
     self.run_list.each{|x| rl << RunListItem.new(x)}
-    rl    
-  end   
-  
+    rl
+  end
+
   def avaiable_recipes
     Cookbook.all.map(&:recipes_name).flatten.map{|x| "recipe[#{x}]"} - self.run_list
-  end 
-  
+  end
+
   def avaiable_roles
     Role.all.map(&:name).flatten.map{|x| "role[#{x}]"} - self.run_list
   end
-  
-  
-  
+
+  def add_to_role_and_save(role)
+    rolename = Role.name_of_role(role)
+    @run_list << rolename unless @run_list.include? rolename
+    save
+  end
+
+  def del_from_role_and_save(role)
+    rolename = Role.name_of_role(role)
+    @run_list.delete(rolename)
+    save
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,7 @@
 class Role < ChefBase
+  allowed_attributes :name, :chef_type, :json_class, :default_attributes, :description, :run_list, :override_attributes
+
   def initialize(attributes={})
-    attributes.reject! { |key, value|
-      ["chef_type", "chef_name"].include? key.to_s
-    }
     attributes = {
       "name"                => nil,
       "chef_type"           => "role",
@@ -16,11 +15,25 @@ class Role < ChefBase
   end
 
   def node_list
-    @node_list || []
+    @node_list || nodes.collect(&:name)
   end
 
-  def nodes
-    Node.search("run_list:role\\[#{self.name}\\]")
+  def node_list=(new_node_list)
+    old_nodes = nodes.collect &:name
+
+    add_role = (new_node_list - old_nodes).collect { |name| Node.find(name) }
+    del_role = (old_nodes - new_node_list).collect { |name|
+      nodes.find { |node| node.name == name }
+    }
+
+    add_role.each { |node| node.add_to_role_and_save(self) }
+    del_role.each { |node| node.del_from_role_and_save(self) }
+  end
+
+  def nodes(use_cache=true)
+    return @nodes if @nodes and use_cache
+    return [] if @name.blank?
+    @nodes = Node.search("role:#{@name}")
   end
 
   def available_roles
@@ -33,5 +46,20 @@ class Role < ChefBase
 
   def extended_run_list
     @extended_run_list || []
+  end
+
+  def self.name_of_role(role)
+    name = case role
+           when Role
+             role.name
+           else
+             role
+           end
+    "role[#{name}]"
+  end
+
+  def self.delete(name)
+    super
+    Node.search("role:#{name}").each { |node| node.del_from_role_and_save(name) }
   end
 end
